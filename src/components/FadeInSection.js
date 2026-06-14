43f3f81fc273d7316683a8e6f57e1108733a03ec
manvendra5345 <singhmanvendra2003@gmail.com>
2026-06-14 19:29:57 +0530

    check

--- src/components/FadeInSection.js
+++ src/components/FadeInSection.js
@@ -2,23 +2,34 @@ import React from "react";
 
 export default function FadeInSection(props) {
   const [isVisible, setVisible] = React.useState(false);
-  const domRef = React.useRef();
+  const domRef = React.useRef(null);
+
   React.useEffect(() => {
-    const currentRef = domRef.current;
-    const observer = new IntersectionObserver(entries => {
-      entries.forEach(entry => {
+    const observer = new IntersectionObserver((entries) => {
+      entries.forEach((entry) => {
         if (entry.isIntersecting) {
-          setVisible(entry.isIntersecting);
+          setVisible(true);
         }
       });
     });
-    observer.observe(currentRef);
-    return () => observer.unobserve(currentRef);
+
+    const currentRef = domRef.current;
+
+    if (currentRef) {
+      observer.observe(currentRef);
+    }
+
+    return () => {
+      if (currentRef) {
+        observer.unobserve(currentRef);
+      }
+    };
   }, []);
+
   return (
     <div
       className={`fade-in-section ${isVisible ? "is-visible" : ""}`}
-      style={{ transitionDelay: `${props.delay}` }}
+      style={{ transitionDelay: props.delay }}
       ref={domRef}
     >
       {props.children}
